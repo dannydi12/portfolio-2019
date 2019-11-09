bbd5287487aa2b1b2369abb40bb526ad14c5d4f8
Daniel Di Venere <danieldivenere@gmail.com>
2019-11-09 15:37:07 -0800

    added content and styling

---
+++ index.js
@@ -0,0 +1,18 @@
+function handleFollowClick() {
+    $('.js-follow').click(function (event) {
+        if($(this).text() == 'Following') {
+            $(this).text('Follow')
+            $(this).removeClass('followed');
+        }
+        else {
+            $(this).text('Following');
+            $(this).addClass('followed');
+        }
+    });
+}
+
+function main() {
+    handleFollowClick();
+}
+
+$(main);
